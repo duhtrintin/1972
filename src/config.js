b35e3e8c6f6399af81a09cebc5b6404cffb2855c
Eduardo Trintin <duh.trintin@gmail.com>
2018-05-07 22:42:56 -0300

    Atualizando o arquivo de configuracao

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb://balta:balta@ds032340.mlab.com:32340/ndstr',
-    sendgridKey: 'SUA CHAVE',
-    containerConnectionString: 'SUA CONNECTION STRING'
+    connectionString: 'mongodb://trintin:trintin@ds014658.mlab.com:14658/ndstr-trintin',
+    sendgridKey: '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',
+    containerConnectionString: 'TBD'
 }
